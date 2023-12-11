fbee67d2252191bf15c6122744cc9700588026ed
JoelSVP <joelsergivp02@gmail.com>
2023-12-11 20:14:27 +0100

    Update React + Node

--- api/app.js
+++ api/app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
+var testAPIRouter = require("./routes/testAPI");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
-var testAPIRouter = require("./routes/testAPI");
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
